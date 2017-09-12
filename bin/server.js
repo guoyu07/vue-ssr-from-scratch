de8f35aa0f551492a27068c274a1aa5542a9b55f
giorgi94 <giorgik1994@gmail.com>
2017-09-12 08:57:06 +0400

    added express server

--- bin/server.js
+++ bin/server.js
@@ -1,29 +1,92 @@
-import path from 'path';
-import express from 'express';
-import webpack from 'webpack';
-import webpackConfig from '../webpack.config';
-import webpackDevMiddleware from 'webpack-dev-middleware';
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const webpackConfig = require('../webpack.config');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+const DIST_DIR = path.join(__dirname, '../dist');
+
+const PORT = process.env.PORT || 8080;
 
 
 const app = express();
+const compiler = webpack(webpackConfig);
+
+if(NODE_ENV === 'development') {  
+    app.use(webpackDevMiddleware(compiler, {
+        publicPath: webpackConfig.output.publicPath
+    }));
+
+    app.use(webpackHotMiddleware(compiler));
 
 
-app.use(webpackDevMiddleware(webpack(webpackConfig)));
+
+    app.get("/", (req, res, next) => {
+        compiler.outputFileSystem.readFile(path.join(DIST_DIR, 'index.html'), (err, result) => {
+            if (err) {
+                return next(err);
+            }
+            res.set('content-type', 'text/html');
+            res.send(result);
+            res.end();
+        });
+    });
+
+    app.get("/about", (req, res, next) => {
+        compiler.outputFileSystem.readFile(path.join(DIST_DIR, 'about/index.html'), (err, result) => {
+            if (err) {
+                return next(err);
+            }
+            res.set('content-type', 'text/html');
+            res.send(result);
+            res.end();
+        });
+    });
+}
+else {  
+
+    console.log('prodaction')
+
+    app.use('/static', express.static(path.join(DIST_DIR, 'static')));
+
+    app.get('/', (req, res)=>{
+        res.sendFile(path.join(DIST_DIR, 'index.html'));
+    });
+
+    app.get('/about', (req, res)=>{
+        res.sendFile(path.join(DIST_DIR, 'about/index.html'));
+    });
+
+
+    // app.get("*", (req, res) => res.sendFile(HTML_FILE));
+}
+
+
+
+
+
+
+
+
+
 
 // app.use('/static', express.static(path.join(__dirname, '../dist/static')));
-app.use('/static', express.static(path.join(__dirname, '../public')));
-
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../dist/index.html'));
-});
-
-app.get('/about', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../dist/about/index.html'));
-});
 
 
-const port = 8080;
+// app.get('/', (req, res)=>{
+//     res.sendFile(path.join(__dirname, '../dist/index.html'));
+// });
 
-app.listen(port, ()=>{
-    console.log(`listening to port ${port}`);
+// app.get('/about', (req, res)=>{
+//     res.sendFile(path.join(__dirname, '../dist/about/index.html'));
+// });
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`listening to port ${PORT}`);
 });
